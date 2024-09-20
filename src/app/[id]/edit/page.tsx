06124763e276db5017296dc58af720a0705d008d
Dustin Lee <dustin.jason.lee@gmail.com>
2024-09-20 12:02:01 -0500

    feat: complete /:id/edit page creation

--- src/app/[id]/edit/page.tsx
+++ src/app/[id]/edit/page.tsx
@@ -1,7 +1,21 @@
-export default function Edit() {
+import TaskForm from "@/components/TaskForm";
+import { getTaskById } from "@/db/actions";
+import { Task } from "@/db/schema";
+import { OperationType } from "@/lib/enums";
+
+interface EditPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function Edit({ params }: EditPageProps) {
+  const { id } = params;
+  const task: Task | null = await getTaskById(Number(id));
+
   return (
     <div className="container flex min-h-screen w-full items-center justify-center p-6">
-      <h1>Edit Page</h1>
+      <TaskForm type={OperationType.Update} defaultValues={task} />
     </div>
   );
 }
