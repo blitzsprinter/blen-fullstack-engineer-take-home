2ea672aa2eef88d199176c780d695c311f7d5250
Dustin Lee <dustin.jason.lee@gmail.com>
2024-09-20 09:48:32 -0500

    init: dynamic routing

--- src/app/[id]/edit/page.tsx
+++ src/app/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Edit() {
   return (
     <div className="container flex min-h-screen w-full items-center justify-center p-6">
-      <h1>Home Page</h1>
+      <h1>Edit Page</h1>
     </div>
   );
 }
--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Details() {
   return (
     <div className="container flex min-h-screen w-full items-center justify-center p-6">
-      <h1>Home Page</h1>
+      <h1>Details Page</h1>
     </div>
   );
 }
